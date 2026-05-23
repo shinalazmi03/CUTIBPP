a04cddfabb58df0f0c5dfc93c6e9e79e260f0d0a
shinalazmi03 <shinalazmi03@gmail.com>
2026-05-23 11:15:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,112 @@
 const SHEET_URL =
 "https://opensheet.elk.sh/17xIifukROSx1w2p98ib3EZNV1uKQCZf8dzgInT1FYXI/CUTI";
 
-const cutiBody = document.getElementById("cutiBody");
+const bengkelAktif =
+document.getElementById("bengkelAktif");
 
-async function loadData() {
+const transportAktif =
+document.getElementById("transportAktif");
 
-  try {
+const semuaCuti =
+document.getElementById("semuaCuti");
 
-    const response = await fetch(SHEET_URL);
-    const data = await response.json();
+async function loadData(){
 
-    cutiBody.innerHTML = "";
+  try{
+
+    const response =
+    await fetch(SHEET_URL);
+
+    const data =
+    await response.json();
 
     data.forEach(item => {
 
-      cutiBody.innerHTML += `
+      const jabatan =
+      item["JABATAN"] || "";
+
+      const rowAktif = `
         <tr>
-          <td>${item["NO"]}</td>
           <td>${item["NAMA"]}</td>
-          <td>${item["NO KARYAWAN"]}</td>
-          <td>${item["JABATAN"]}</td>
+          <td>${jabatan}</td>
           <td>${item["CUTI DARI"]}</td>
           <td>${item["CUTI SAMPAI"]}</td>
-          <td>${item["KETERANGAN"]}</td>
-          <td>${item["ALAMAT cUTI"]}</td>
-          <td>${item["DI BUAT"]}</td>
-          <td>${item["DI PAKAI"]}</td>
         </tr>
       `;
 
+      const rowSemua = `
+        <tr>
+          <td>${item["NAMA"]}</td>
+          <td>${jabatan}</td>
+          <td>${item["KETERANGAN"]}</td>
+          <td>${item["ALAMAT cUTI"]}</td>
+        </tr>
+      `;
+
+      // BENGKEL
+
+      if(
+        jabatan.toLowerCase().includes("mek") ||
+        jabatan.toLowerCase().includes("bengkel")
+      ){
+        bengkelAktif.innerHTML += rowAktif;
+      }
+
+      // TRANSPORT
+
+      else{
+        transportAktif.innerHTML += rowAktif;
+      }
+
+      semuaCuti.innerHTML += rowSemua;
+
     });
 
-  } catch (error) {
+  }catch(error){
 
     console.log(error);
-
-    cutiBody.innerHTML = `
-      <tr>
-        <td colspan="10">
-          Gagal mengambil data spreadsheet
-        </td>
-      </tr>
-    `;
 
   }
 
 }
 
 loadData();
+
+/* FORM WA */
+
+document
+.getElementById("cutiForm")
+.addEventListener("submit", function(e){
+
+  e.preventDefault();
+
+  const nama =
+  document.getElementById("nama").value;
+
+  const dari =
+  document.getElementById("dari").value;
+
+  const sampai =
+  document.getElementById("sampai").value;
+
+  const keperluan =
+  document.getElementById("keperluan").value;
+
+  const pesan =
+`PENGAJUAN CUTI BPP
+
+Nama : ${nama}
+
+Cuti Dari : ${dari}
+
+Cuti Sampai : ${sampai}
+
+Keperluan :
+${keperluan}`;
+
+  const url =
+`https://api.whatsapp.com/send/?phone=6282288313424&text=${encodeURIComponent(pesan)}`;
+
+  window.open(url, "_blank");
+
+});
